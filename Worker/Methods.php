b3b18985037dbf2ba2db8541f566b89af4793e7e
Christian Lück <christian@lueck.tv>
2011-05-16 19:09:38 +0200

    Helper class managing methods

---
+++ Worker/Methods.php
@@ -0,0 +1,42 @@
+<?php
+
+class Worker_Methods{
+    private $methods;
+    
+    public function __construct($methods=array()){
+        $this->methods = array();
+    }
+    
+    public function hasMethod($name){
+        return isset($this->methods[$name]);
+    }
+    
+    public function getMethodNames(){
+        return array_keys($this->methods);
+    }
+    
+    public function addMethod($name,$fn){
+        $this->methods[$name] = $fn;
+    }
+    
+    public function addMethods($methods){
+        if(!is_array($methods)){
+            $methods = Worker_Methods::extract($methods);
+        }
+        foreach($methods as $name=>$fn){
+            $this->methods[$name] = $fn;
+        }
+        return $methods;
+    }
+    
+    public function call($name,$args){
+        if(!isset($this->methods[$name])){
+            throw new Worker_Exception('Given method not callable');
+        }
+        return call_user_func_array($this->methods[$name],$args);
+    }
+    
+    public static function extract($class){
+        return array();
+    }
+}
