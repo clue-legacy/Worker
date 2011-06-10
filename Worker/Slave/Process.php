7c9cf8ad48a3aba768fcaf30666d3ec851de5c6a
Christian Lück <christian@lueck.tv>
2011-06-10 17:38:34 +0200

    Avoid duplicate fclose calls to Process pipes

--- Worker/Slave/Process.php
+++ Worker/Slave/Process.php
@@ -30,7 +30,6 @@ class Worker_Slave_Process extends Worker_Slave{
     }
     
     public function close(){
-        parent::close();
         $this->process->kill(true)->close(true);
         unset($this->process);
     }
