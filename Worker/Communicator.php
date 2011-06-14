50b05c34a7aad4a98c3ef9fa80d5058460e70c5a
Christian Lück <christian@lueck.tv>
2011-06-14 14:34:28 +0200

    Add missing communicator base class

---
+++ Worker/Communicator.php
@@ -0,0 +1,22 @@
+<?php
+
+abstract class Worker_Communicator{
+    /**
+     * get stream to read from
+     * 
+     * @return resource
+     */
+    abstract public function getStreamRead();
+    
+    /**
+     * get stream to write to
+     * 
+     * @return resource
+     */
+    abstract public function getStreamWrite();
+    
+    /**
+     * close all communication streams
+     */
+    abstract public function close();
+}
