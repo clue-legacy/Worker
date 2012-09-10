54f0486df4c844ab4152dda782ee0030aaee16eb
Christian Lück <christian@lueck.tv>
2012-09-10 16:20:25 +0200

    Use composer autoloader instead of custom implementation

--- example/simple-rpc/master.php
+++ example/simple-rpc/master.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__).'/../init.inc.php');
+require_once __DIR__.'/../../vendor/autoload.php';
 
 $master = new Worker_Master();
 $master->setDebug(true);
--- example/simple-rpc/slave.php
+++ example/simple-rpc/slave.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__).'/../init.inc.php');
+require_once __DIR__.'/../../vendor/autoload.php';
 
 $master = Worker_Master::connect()->decorateMethods();
 
